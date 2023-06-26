2a61cf805cfac051abd1f2b1b1bf40a847e7d66a
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-06-26 09:47:01 -0500

    Create modules.c

---
+++ samples/modules.c
@@ -0,0 +1,10 @@
+#include "open_ai"
+#include "module.h"
+#include <cmath>
+#include <algorithm.h>
+#include <string.h>
+
+using namespace str;
+module = train(0) * AskAI
+   
+# I'll finish this module at home 
