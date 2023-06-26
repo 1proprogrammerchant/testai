94a8ddf1f9d061e9fcd4f3491090e42cfbef6838
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-06-26 09:56:34 -0500

    Create module.cpp

---
+++ samples/module.cpp
@@ -0,0 +1,22 @@
+#include "module.h"
+#include "overload.h"
+#include "pyplot.pyi"
+#include <constexpr.h>
+
+
+using namespace std;
+[
+  int = 1
+  int a = 0
+  int b = 0
+  int c = 1
+]
+#prgma conclude 
+
+matrices = overcrap(0)
+  {[0,1,0
+  0,1,0,1
+  0,1,0,0
+  0,1,1,0}]
+// test for the module to see if it can manage a simple matrices
+void f(int& i)
