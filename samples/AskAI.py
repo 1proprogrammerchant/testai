b635cbdc8007676e70db48744315252108b12731
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-06-26 09:43:34 -0500

    Create AskAI.py

---
+++ samples/AskAI.py
@@ -0,0 +1,13 @@
+from open_ai import ask_ai
+
+# machine module trainer
+ask_ai = ask(0)
+
+
+# the question
+question_ai = "What is the capital of Frace"
+
+# the answer
+response_ai = (question) *(0)
+
+response_ai = (answer)
